7764f71560aad5978efc56e76955aa64f029b6c3
nicoromero <nicoromero07@hotmail.com>
2022-03-09 17:33:08 +0100

    fibonacci.java

--- src/main/java/org/nicor/Fibonacci.java
+++ src/main/java/org/nicor/Fibonacci.java
@@ -1,7 +1,22 @@
 package org.nicor;
 
 public class Fibonacci {
-    public int compute(){
-        return 0;
+    public int compute(int n){
+        if(n<0){
+            throw new RuntimeException("El valor es negativo: " + n);
+        }
+        if(n <= 1) {
+            return n;
+        }
+
+        int fib = 1;
+        int prevFib = 1;
+
+        for(int i=2; i<n; i++) {
+            int temp = fib;
+            fib+= prevFib;
+            prevFib = temp;
+        }
+        return fib;
     }
 }
